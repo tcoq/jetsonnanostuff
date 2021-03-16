e63fed560da8cc820a6366df19cd1ee1f9b89237
Steffen Otto <steffen.otto@dologo.de>
2021-03-16 07:53:33 +0100

    Update search-object.py
    Some improvements

--- search-object.py
+++ search-object.py
@@ -22,6 +22,7 @@ logging.info("Init done -------------------------")
 # set path for folders where source/ raw videos could be found and where processed should be moved
 pathRaw = "/myscripts/rawvideo/"
 pathProcessed = "/myscripts/processedvideos/"
+pathError = "/myscripts/errorvideos/"
 
 while True:
     dirs = listdir(pathRaw)
@@ -42,20 +43,42 @@ while True:
                 if (len(detections) > 0):
                     for detection in detections:
                         classid = detection.ClassID
-                        confidence = detection.Confidence
-                        # ClassID 1 means person, 17 = cat, 18 = dog and belongs to loaded model ssd-mobilenet-v2
-                        if (confidence > 0.79): 
-                            # now we found something with 79% propability
+                        confidence = round(detection.Confidence,2)
+                        topD = int(detection.Top)
+                        leftD = int(detection.Left)
+                        rightD = int(detection.Right)
+                        widthD = int(detection.Width)
+
+                        # specify areas in video-image to avoid detection (y axis pixel)
+                        topNoDetectionZone = 140
+                        if (topD < topNoDetectionZone):
+                            continue
+                        # classids belong to model ssd-mobilenet-v2
+                        # 1 = person
+                        # 17 = cat
+                        # 18 = dog
+
+                        # 49 = knife / occurs on rain
+                        # 61 = cake / occurs on wind
+                        # this classids produce false positives...
+                        if (classid == 49 or classid == 61):
+                            continue
+
+                        if (confidence >= 0.78): 
+                            # now we found something with n% propability
                             # count times objects where found
                             frameCnt = frameCnt + 1
 
-                            if (frameCnt > 3):
+                            if (frameCnt > 5):
                                 # stop analyzing if object was found more than n times to avoid false positives
-                                logging.info('Detected classID: ' + str(classid) + ' with confidence: ' + str(confidence) + " in : " + fil)
-                                processedFilename = pathProcessed + "cid_" + str(classid) + "_cf_" + str(confidence) + "_" + fil
+                                logging.info('Detected classID: ' + str(classid) + ',confidence: ' + str(confidence) + ',coordinates: (top=' + str(topD) + ',left=' + str(leftD) + ',rigth=' + str(rightD) + ',width='  + str(widthD) + ') in imagesize (' + str(img.width) + ', ' + str(img.height) + ') file: ' + fil)
+                                processedFilename = pathProcessed + "cid_" + str(classid) + "_cf_" + str(confidence) + "_top_" + str(topD) + "_" + fil
                                 shutil.move(filename, processedFilename)
                                 objectFound = True
                                 break
+                        else:
+                            # we want consecutive frames
+                            frameCnt = 0
                 # check if object found or video-stream is at the end or object found
                 if (not input.IsStreaming() or objectFound): 
                     # check if file was already moved
@@ -63,13 +86,13 @@ while True:
                         try:
                             remove(filename) 
                         except:
-                            logging.warn("Cannot delete: " + filename + " , unexpected error: " + str(sys.exc_info()[0]))
+                            logging.exception("Cannot delete: " + filename + " , unexpected error: ")
                     objectFound = False
                     break
         except:
-            logging.error("Exception reading file: " + filename + " , unexpected error: " + str(sys.exc_info()[0]))
+            logging.exception("Exception reading file: " + filename + " , unexpected error: ")
+            shutil.move(filename, pathError + fil)
 
     # wait 10 seconds for new files
     sleep(10)
 logging.info("Shutdown -------")
-
